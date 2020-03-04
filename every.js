e6c5ca199020b705e90bd10dcadce3d868d10fd1
FrancescoZingarelli <59926996+FrancescoZingarelli@users.noreply.github.com>
2020-03-04 02:52:45 +0100

    Update every.js

--- every.js
+++ every.js
@@ -1,1 +1,11 @@
+let everyBase = function(array, callback) {
+    for (let element of array) {
+        if (!callback(element)) return false;
+    }
+    return true;
+};
 
+let everyReduce = (array, callback) => array.reduce((a, b) => !a ? false : callback(b), true);
+
+console.log(everyBase([2, 4, 6], x => x % 2 === 0), everyBase([1, 2, 3], x => x % 2 === 0));
+console.log(everyReduce([2, 4, 6], x => x % 2 === 0), everyReduce([1, 2, 3], x => x % 2 === 0));
