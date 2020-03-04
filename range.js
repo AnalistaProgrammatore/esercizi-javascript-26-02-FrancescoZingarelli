e33bc1a780ecf632be2200cc234733286d10e909
FrancescoZingarelli <59926996+FrancescoZingarelli@users.noreply.github.com>
2020-03-04 02:03:21 +0100

    Update range.js

--- range.js
+++ range.js
@@ -1,3 +1,32 @@
-/**
-* Scrivere qui il codice dell'esercizio 1 del capitolo 4 di eloquentJS
-**/
+// CON FROM NON SO COME FARLA......................
+
+
+// PARAMETRI POSIZIONALI
+
+let rangePos = function(start, stop, step = 1) {
+    let result = [];
+    for (let i = start; i <= stop; i += step) {
+        result = result.concat(i);
+    }
+    return result;
+}
+
+
+
+// PARAMETRI NOMINALI
+
+let rangeNom = function({start, stop, step = 1}) {
+    let result = [];
+    for (let i = start; i <= stop; i += step) {
+        result = result.concat(i);
+    }
+    return result;
+}
+
+console.log(rangeNom({stop: 10, start:1, step: 2}));
+
+
+
+// SUM OF RANGE
+
+console.log(rangePos(1, 3, 1).reduce((a, b) => a + b));
