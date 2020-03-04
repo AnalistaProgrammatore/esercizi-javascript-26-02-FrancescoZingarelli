241e6055b4194b9084a915ce1f16f4411720c15e
FrancescoZingarelli <59926996+FrancescoZingarelli@users.noreply.github.com>
2020-03-04 02:44:50 +0100

    Update filter.js

--- filter.js
+++ filter.js
@@ -1,1 +1,13 @@
+let filterBase = function(array, callback) {
+    let result = []
+    for (let element of array) {
+    if (callback(element)) result = result.concat(element);
+    }
+    return result
+};
 
+let filterReduce = (array, callback) => array.reduce((a, b) => callback(b) ? a.concat(b) : a, [])
+
+
+console.log(filterBase([1, 2, 3, 4, 5, 6], x => x % 2 === 0));
+console.log(filterReduce([1, 2, 3, 4, 5, 6], x => x % 2 === 0));
