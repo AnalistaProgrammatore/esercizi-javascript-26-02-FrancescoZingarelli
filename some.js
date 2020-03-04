75128481b19294fb5e8e4f13e2366feb9c3141d4
FrancescoZingarelli <59926996+FrancescoZingarelli@users.noreply.github.com>
2020-03-04 02:22:14 +0100

    Update some.js

--- some.js
+++ some.js
@@ -1,1 +1,13 @@
+let someBase = function(array, callback) {
+    for (let element of array) {
+        if (callback(element)) return true;
+    }
+    return false;
+};
 
+let someReduce = (array, callback) => array.reduce((a, b) => a ? true : callback(b), false);
+
+console.log(someBase([1, 2, 3], x => x > 2));
+console.log(someBase([1, 2, 3], x => x > 4));
+console.log(someReduce([1, 2, 3], x => x > 2));
+console.log(someReduce([1, 2, 3], x => x > 4));
