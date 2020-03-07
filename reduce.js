d857a039f1b99b9bdeebce6dad3b7b7ea9e7d514
FrancescoZingarelli <59926996+FrancescoZingarelli@users.noreply.github.com>
2020-03-07 11:00:45 +0100

    Update reduce.js

--- reduce.js
+++ reduce.js
@@ -1,1 +1,20 @@
+let reduceBase = function(array, callback, start) {  
+    let acc;
+    if (!start) {
+        acc = array[0];
+        for (let i = 1; i < array.length; i++) {
+            acc = callback(acc, array[i]);
+        }
+    } else {
+        acc = start;
+        for (let i = 0; i < array.length; i++) {
+            acc = callback(acc, array[i]);
+        }
+    }
+    return acc;
+}
 
+
+
+console.log(reduceBase([1, 2, 3, 4], (acc, curr) => acc + curr));
+console.log(reduceBase([1, 2, 3, 4], (acc, curr) => acc + curr, 100));
